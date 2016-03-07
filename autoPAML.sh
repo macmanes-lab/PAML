0c5a0fdbb56c997951e26046aabfb6a3315f0205
Matt MacManes <macmanes@gmail.com>
2016-03-07 15:47:51 -0500

    fi missing

--- autoPAML.sh
+++ autoPAML.sh
@@ -27,13 +27,14 @@ for inputaln in $(ls *fasta); do
         echo 'I have a core to use'
         java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq "$inputaln" -out_NT $F.aln &&
         perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &&
-        if [-s $F.clean ] ;
+        if [ -s $F.clean ] ;
         then
             raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
             python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
             python autoPAMLresults.py $F.out | tee -a paml.results &
         else
             echo 'BOOM!!!'
+        fi
     else
         echo 'Dont wake me up until there is something else to do'
         sleep 25s
