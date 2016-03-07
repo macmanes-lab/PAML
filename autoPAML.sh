6ebadda1cf831948850be4786969e1d5170d6370
Matt MacManes <macmanes@gmail.com>
2016-03-07 15:55:52 -0500

    and statement

--- autoPAML.sh
+++ autoPAML.sh
@@ -27,7 +27,7 @@ for inputaln in $(ls *fasta); do
         echo 'I have a core to use'
         java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq "$inputaln" -out_NT $F.aln &&
         perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &&
-        if [ -s $F.clean ] ;
+        if [ -s $F.clean -a $(ps -all | grep 'java\|codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
         then
             raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
             python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
