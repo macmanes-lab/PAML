7a1ad49202510e0f166a58500f9f6148c93d8cbd
Matt MacManes <macmanes@gmail.com>
2016-03-10 06:52:33 -0500

    break up into 2 loops

--- autoPAML.sh
+++ autoPAML.sh
@@ -22,19 +22,25 @@ done
 
 for inputaln in $(ls *fasta); do
     F=$(basename "$inputaln" .fasta)
-    if [ $(ps -all | grep 'java\|codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
+    if [ $(ps -all | grep 'java' | wc -l | awk '{print $1}') -lt $TC ] ;
     then
         echo 'I have a core to use'
         java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq "$inputaln" -out_NT $F.aln &&
-        perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &&
-        if [ -s $F.clean && $(ps -all | grep 'java\|codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
-        then
-            raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
-            python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
-            python autoPAMLresults.py $F.out | tee -a paml.results &
-        else
-            echo 'BOOM!!!'
-        fi
+        perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean &
+    else
+        echo 'Dont wake me up until there is something else to do'
+        sleep 5s
+    fi
+done
+
+for inputaln in $(ls *fasta); do
+    F=$(basename "$inputaln" .fasta)
+    if [ -s $F.clean -a $(ps -all | grep 'codeml\|raxmlHPC' | wc -l | awk '{print $1}') -lt $TC ] ;
+    then
+        echo 'I have a core to use'
+        raxmlHPC-PTHREADS -f a -m GTRCAT -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
+        python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
+        python autoPAMLresults.py $F.out | tee -a paml.results &
     else
         echo 'Dont wake me up until there is something else to do'
         sleep 25s
