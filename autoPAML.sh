6d64bd4015f9e5ea62ea14291ab0f6ae5df87d62
Matt MacManes <matthew.macmanes@unh.edu>
2016-07-05 07:31:09 -0400

    add random and T2 for raxml

--- autoPAML.sh
+++ autoPAML.sh
@@ -29,7 +29,7 @@ for inputaln in $(ls *fasta); do
             echo 'processing' $inputaln
             java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq "$inputaln" -out_NT $F.aln &&
             perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean || true &&
-            raxmlHPC-PTHREADS -f a -m GTRGAMMA -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
+            raxmlHPC-PTHREADS -f a -m GTRGAMMA -T 2 -x $RANDOM -N 100 -n $F.tree -s $F.clean -p $RANDOM &&
             python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
             python autoPAMLresults.py $F.out | tee -a paml.results &
         else
@@ -45,7 +45,7 @@ for inputaln in $(ls *fasta); do
         then
             java -Xmx2000m -jar /share/bin/macse_v1.01b.jar -prog alignSequences -seq "$inputaln" -out_NT $F.aln &&
             perl /share/pal2nal.v14/pal2nal.pl $F'_macse_AA.fasta' $F.aln -output fasta -nogap -nomismatch > $F.clean || true &&
-            raxmlHPC-PTHREADS -f a -m GTRGAMMA -T 1 -x 340394856 -N 100 -n $F.tree -s $F.clean -p 69283650 &&
+            raxmlHPC-PTHREADS -f a -m GTRGAMMA -T 2 -x $RANDOM -N 100 -n $F.tree -s $F.clean -p $RANDOM &&
             python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
             python autoPAMLresults.py $F.out | tee -a paml.results &
         else
