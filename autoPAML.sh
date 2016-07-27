5889f033e4577b5c7587f1b1b0b7c5f41e179f98
Matt MacManes <matthew.macmanes@unh.edu>
2016-07-27 09:26:12 -0400

    make regex to mod tree file for branch site

--- autoPAML.sh
+++ autoPAML.sh
@@ -6,15 +6,17 @@ usage=$(cat << EOF
 
    autoPAML.sh [options]
    Options:
-      -t <v> : *required* Numberof threads to use.
+      -t <v> : *required* Number of threads to use.
+      -l <v> : *required* Code for foreground lineage (must work in regex)
 EOF
 );
 
-while getopts o:t: option
+while getopts l:t: option
 do
         case "${option}"
         in
 		t) TC=${OPTARG};;
+		l) LI=${OPTARG};;		
         esac
 done
 
@@ -31,6 +33,7 @@ for inputaln in $(ls *fasta); do
             prank -d="$inputaln" -translate -F -o=$F &&
             perl /share/pal2nal.v14/pal2nal.pl $F.best.pep.fas $F.best.nuc.fas -output fasta -nogap -nomismatch > $F.clean || true &&
             raxmlHPC-PTHREADS -f a -m GTRGAMMA -T 2 -x $RANDOM -N 100 -n $F.tree -s $F.clean -p $RANDOM &&
+            sed -i -r 's/$LI_[0-9]{2,}-RA:[0-9]{1,}.[0-9]{2,}/& #1/g' RAxML_bestTree.$F.tree
             python autoPAML.py $F.clean RAxML_bestTree.$F.tree $F.out &&
             python autoPAMLresults.py $F.out | tee -a paml.results &
         else
